8a755a65002e42d32ec4ed922f86bf8c077c8a8a
MeikoFudo <akokoeva000@gmail.com>
2023-01-09 11:47:12 +0300

    Добавьте файлы проекта.

---
+++ MyMod/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MyMod
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(265, 131);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(145, 43);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Вычислить";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(265, 61);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(145, 44);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxShow
+            // 
+            this.textBoxShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxShow.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBoxShow.Location = new System.Drawing.Point(265, 259);
+            this.textBoxShow.Name = "textBoxShow";
+            this.textBoxShow.Size = new System.Drawing.Size(145, 62);
+            this.textBoxShow.TabIndex = 2;
+            this.textBoxShow.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxShow);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.Calculate);
+            this.Name = "FormGame";
+            this.Text = "FormGame";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxShow;
+    }
+}
+
---
+++ MyMod/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMod
+{
+
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxInput.Text != "")
+                if (int.TryParse(textBoxInput.Text, out _))
+                    textBoxShow.Text = "4k" + "+" +(int.Parse(textBoxInput.Text) % 4);
+                else
+                    MessageBox.Show("Введите число");
+            else
+                MessageBox.Show("Введите число");
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
